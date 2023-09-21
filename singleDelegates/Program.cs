b46e52c048589d8e025045c55fc73879da91978d
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-21 19:06:41 +0530

    Add project files.

---
+++ singleDelegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleDelegates
+{
+    public delegate void Cal(int a, int b);
+     class Program
+    {
+       public static void ADD(int a,int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result+"Rrsult of Add:");
+       
+        }
+        static void Main(string[] args)
+        {
+            Cal obj = new Cal(ADD);
+            obj(67,67);
+            Console.ReadLine
+                ();
+        }
+    }
+}
